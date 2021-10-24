99a6fc0573ccbdbfbf679800de0c99740aa8f1ee
mohamedtamer0 <mt4452242@gmail.com>
2021-10-24 17:47:22 +0200

    update

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -11,8 +11,16 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             println("URI: ${call.request.uri}")
-            println("Headers: ${call.request.headers}")
-                call.respondText("Hello Ktor!")
-            }
+            println("Headers: ${call.request.headers.names()}")
+
+            println("User-Agent: ${call.request.headers["User-Agent"]}")
+            println("Accept: ${call.request.headers["Accept"]}")
+            println("Query Params: ${call.request.queryParameters.names()}")
+            println("Name: ${call.request.queryParameters["name"]}")
+            println("Email: ${call.request.queryParameters["email"]}")
+
+
+            call.respondText("Hello Ktor!")
+        }
     }
 }
