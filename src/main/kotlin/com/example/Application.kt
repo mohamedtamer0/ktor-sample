d7bb2cd9b0ea7981fd3503810e655008e904894d
mohamedtamer0 <mt4452242@gmail.com>
2021-10-24 16:31:47 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        configureRouting()
+    }.start(wait = true)
+}
