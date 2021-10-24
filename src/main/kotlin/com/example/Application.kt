1d25b32265e7168dc27ab157afa0604ab8f78aac
mohamedtamer0 <mt4452242@gmail.com>
2021-10-24 16:43:08 +0200

    update

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         configureRouting()
     }.start(wait = true)
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,9 @@
 package com.example.plugins
 
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+
 
 fun Application.configureRouting() {
 
