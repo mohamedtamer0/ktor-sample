da3ceaf7d85c5e44539e8d03939f3260969fdb6d
mohamedtamer0 <mt4452242@gmail.com>
2021-10-26 03:37:21 +0200

    update

--- src/main/kotlin/com/example/routing/NotesRoutes.kt
+++ src/main/kotlin/com/example/routing/NotesRoutes.kt
@@ -89,5 +89,65 @@ fun Application.notesRoutes() {
         }
 
 
+        // UPDATE A Note
+        put("/notes/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: -1
+            val updateNote = call.receive<NoteRequest>()
+
+            val rowsEffected = db.update(NotesEntity) {
+                set(it.note, updateNote.note)
+                where {
+                    it.id eq id
+                }
+            }
+
+            if (rowsEffected == 1) {
+                call.respond(
+                    HttpStatusCode.OK,
+                    NoteResponse(
+                        success = true,
+                        data = "Note has been Update"
+                    )
+                )
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data = "Note Failed to Update"
+                    )
+                )
+            }
+        }
+
+
+        // DELETE A Note
+        delete("/notes/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: -1
+            val rowsEffected = db.delete(NotesEntity) {
+                it.id eq id
+            }
+
+            if (rowsEffected == 1) {
+                call.respond(
+                    HttpStatusCode.OK,
+                    NoteResponse(
+                        success = true,
+                        data = "Note has been Delete"
+                    )
+                )
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data = "Note Failed to Delete"
+                    )
+                )
+            }
+
+        }
+
+
     }
 }
