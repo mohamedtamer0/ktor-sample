14eb122b357f56e7dd2e5e0ecf9fff4ff2077e45
mohamedtamer0 <mt4452242@gmail.com>
2021-10-26 02:20:25 +0200

    update

--- src/main/kotlin/com/example/routing/NotesRoutes.kt
+++ src/main/kotlin/com/example/routing/NotesRoutes.kt
@@ -10,16 +10,15 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.ktorm.dsl.from
-import org.ktorm.dsl.insert
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
+import org.ktorm.dsl.*
 
 
 fun Application.notesRoutes() {
     val db = DatabaseConnection.database
 
     routing {
+
+
         get("/notes") {
             val notes = db.from(NotesEntity).select()
                 .map {
@@ -30,6 +29,8 @@ fun Application.notesRoutes() {
             call.respond(notes)
         }
 
+
+        //// Inserting Notes into DB With a Post Request
         post("/notes") {
             val request = call.receive<NoteRequest>()
             val result = db.insert(NotesEntity) {
@@ -52,6 +53,41 @@ fun Application.notesRoutes() {
                 )
             }
         }
+
+
+        // GET Note By ID
+        get("/notes/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: -1
+
+            val note = db.from(NotesEntity)
+                .select()
+                .where { NotesEntity.id eq id }
+                .map {
+                    val id = it[NotesEntity.id]!!
+                    val note = it[NotesEntity.note]!!
+                    Note(id = id, note = note)
+                }.firstOrNull()
+
+            if (note == null) {
+                call.respond(
+                    HttpStatusCode.NotFound,
+                    NoteResponse(
+                        success = false,
+                        data = "Could not found note with id = $id"
+                    )
+                )
+            } else {
+                call.respond(
+                    HttpStatusCode.OK,
+                    NoteResponse(
+                        success = true,
+                        data = note
+                    )
+                )
+            }
+
+        }
+
 
     }
 }
